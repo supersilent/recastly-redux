64b0c97c2ca6af977a983bd3fda7b5b8b9bb1e1c
supersilent <shrikethrush@gmail.com>
2018-11-28 20:49:54 -0800

    Temporal commit.

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './../reducers/main.js';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./../reducers/main.js";
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
