c456b18b320bd43b8ee73147482201aeadeb22e1
supersilent <shrikethrush@gmail.com>
2018-11-28 18:24:31 -0800

    temporal commit

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,12 +1,19 @@
-import searchYouTube from '../lib/searchYouTube.js';
-import changeVideoList from './videoList.js';
-import changeVideo from './currentVideo.js';
-import YOUTUBE_API_KEY from '../config/youtube.js';
+import searchYouTube from "../lib/searchYouTube.js";
+import changeVideoList from "./videoList.js";
+import changeVideo from "./currentVideo.js";
+import YOUTUBE_API_KEY from "../config/youtube.js";
 
-
-var handleVideoSearch = (q) => {
- 
+var handleVideoSearch = q => {
   //TODO:  Write an asynchronous action to handle a video search!
+
+  return dispatch => {
+    console.log({key: YOUTUBE_API_KEY, query: q, max: 5},);
+    searchYouTube({key: YOUTUBE_API_KEY, query: q, max: 5}, function(items) {
+      dispatch(changeVideo(items[0]));
+      dispatch(changeVideoList(items));
+    });
+    
+  };
 };
 
 export default handleVideoSearch;
--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,6 @@
-var changeVideoList = (videos) => ({
-  //TODO:  Return some action object to change the list of videos in VideoList.
+var changeVideoList = (videos) => ( {
+  type: 'CHANGE_VIDEO_LIST',
+  videos
 });
 
 export default changeVideoList;
